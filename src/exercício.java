b257a3e7bac7273299d3b999403e55a3e184fd96
Gabriel_Augusto-Gustavo-Araujo <Gabriel_Augusto-Gustavo-Araujo@exemple.com>
2026-03-23 08:24:03 -0300

    Entrega de exercício 1

---
+++ src/exercício.java
@@ -0,0 +1,9 @@
+public class exercício {
+
+    int series;
+    int repeticoes;
+    double carga;
+    String dificuldade;
+    String musculo;
+
+}
---
+++ src/receita.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+
+public class receita {
+    String titulo;
+    String autor;
+    double avalicao;
+    String ingredientes;
+    LocalTime duracao;
+    double preco;
+    int porcao;
+    String dificuldade;
+
+}
