73c1dc4b8674e5524f63fe51f126949ff11afc10
GustavoA-GabrielA <gustavoaraujo13001@gmail.com>
2026-04-12 23:26:59 -0300

    Alteracao de classes relacionadas ao exercício físico

---
+++ Exercicio/enums/Musculo.java
@@ -0,0 +1,17 @@
+package Exercicio.enums;
+
+public enum Musculo {
+    PEITORAL,
+    COSTAS,
+    PERNAS,
+    QUADRICEPS,
+    GLUTEOS,
+    TRAPEZIO,
+    OBLIQUOS,
+    ANTEBRAÇO,
+    ABDOMEM,
+    SERRATIL,
+    OMBROS,
+    BICEPS,
+    TRICEPS;
+}
---
+++ Exercicio/model/Exercicio.java
@@ -0,0 +1,16 @@
+package Exercicio.model;
+
+import Exercicio.enums.Musculo;
+import Exercicio.enums.TipoExercicio;
+
+public class Exercicio {
+        public int series;
+        public int repeticoes;
+        public double carga;
+        public String dificuldade;
+        public Musculo musculo;
+        public String detalhes;
+        public String descricao;
+        public TipoExercicio tipo;
+
+}
